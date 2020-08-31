a720689a4a917fed709de9c0d9cc634eaa3aea6e
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-08-31 23:16:58 +0000

    Mumuki Export on 2020-08-31 23:16:58 +0000

--- 00001_Creando a Pepita/test.py
+++ 00001_Creando a Pepita/test.py
@@ -1,11 +1,10 @@
-#...content...#
-
 class Test(unittest.TestCase):
-    def test_00001_Pepita_existe(self):
-        nuevo_identificador = "Pepita"
-        self.assertIn(nuevo_identificador, dir(),"{} tiene que existir (con ese 'identificador', mayúsculas/minúsculas, etc)".format(nuevo_identificador))
+  #...content...#
+  def test_00001_Pepita_existe(self):
+      nuevo_identificador = "Pepita"
+      self.assertIn(nuevo_identificador, dir(),"{} tiene que existir (con ese 'identificador', mayúsculas/minúsculas, etc)".format(nuevo_identificador))
 
 
-    def test_00002_Pepita_es_un_objeto(self):
-        nuevo_identificador = "Pepita"
-        self.assertIsInstance(Pepita, objeto, "'{}' tiene que ser del tipo 'objeto'".format(nuevo_identificador))
+  def test_00002_Pepita_es_un_objeto(self):
+      nuevo_identificador = "Pepita"
+      self.assertIsInstance(Pepita, objeto, "'{}' tiene que ser del tipo 'objeto'".format(nuevo_identificador))
