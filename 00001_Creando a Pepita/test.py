d50ab22ca82c48bb33f74ab6e9247e67aa79f9cd
Leandro Batlle <leandro.batlle@gmail.com>
2020-08-31 19:57:55 -0300

    Update test.py

--- 00001_Creando a Pepita/test.py
+++ 00001_Creando a Pepita/test.py
@@ -1,11 +1,9 @@
 class Test(unittest.TestCase):
     def test_00001_Pepita_existe(self):
-        Pepita = objeto()
         nuevo_identificador = "Pepita"
         self.assertIn(nuevo_identificador, dir(),"{} tiene que existir (con ese 'identificador', mayúsculas/minúsculas, etc)".format(nuevo_identificador))
 
 
     def test_00002_Pepita_es_un_objeto(self):
-        Pepita = objeto()
         nuevo_identificador = "Pepita"
         self.assertIsInstance(Pepita, objeto, "'{}' tiene que ser del tipo 'objeto'".format(nuevo_identificador))
