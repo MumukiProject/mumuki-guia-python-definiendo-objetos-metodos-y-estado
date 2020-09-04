ed7bd354c18729eaae56a3aa88c3ab9bb658c070
Leandro Batlle <leandro.batlle@gmail.com>
2020-09-04 18:33:34 -0300

    retoco

--- 00003_Los mejores, los únicos, los métodos en objetos/test.py
+++ 00003_Los mejores, los únicos, los métodos en objetos/test.py
@@ -18,14 +18,14 @@ class Test(unittest.TestCase):
 
     def test_interfaz_de_Pepita_tiene_todos_los_mensajes_que_entiende(self):
         interfaz_esperada = ["cantar()", "comer_lombriz()", "volar_en_circulos()"]
-        interfaz_entregada = interfaz_Pepita
+        interfaz_entregada = dir(Pepita)
 
         self.assertEqual(set(interfaz_esperada), set(interfaz_entregada),
                          "la interfaz de {nombre} es algo como {iface}".format(nombre="Pepita", iface=interfaz_ejemplo))
 
     def test_interfaz_de_Pepita_con_palabras_en_minusculas(self):
         interfaz_esperada = ["cantar()", "comer_lombriz()", "volar_en_circulos()"]
-        interfaz_entregada = interfaz_Pepita
+        interfaz_entregada = dir(Pepita)
         todos_minusculas = all([str(x).islower() for x in interfaz_entregada])
         self.assertTrue(todos_minusculas,
                         mayusminus + " La interfaz de {nombre} tiene todas palabras en minúsculas (que pueden estar unidas por espacios subrayados), es algo como {iface}".format(
