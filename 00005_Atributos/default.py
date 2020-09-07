00079fac456f9b82a6e999980e024f57f6022b37
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-09-07 20:07:11 +0000

    Mumuki Export on 2020-09-07 20:07:11 +0000

--- 00005_Atributos/default.py
+++ 00005_Atributos/default.py
@@ -4,9 +4,10 @@ Pepita.energia = 100
 
 def volar_en_circulos(self):   
   self.energia = self.energia - 10
-  pass #ultima línea del procedimiento (pass no tiene efecto, lo usamos principalmente para bloques vacios)
+  #pass no tiene efecto, marca para bloques vacios
+  pass #ultima línea del procedimiento 
 
-# (fin del proceidmiento por la indentación)
+# (fin del procedimiento por la indentación)
 # Seguí por acá...
 
 Pepita.volar_en_circulos = volar_en_circulos
