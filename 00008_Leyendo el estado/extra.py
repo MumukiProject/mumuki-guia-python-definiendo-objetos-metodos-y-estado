bd5620a48473004da8e7c0cd0c7c2ef44ffa6c14
Leandro Batlle <leandro.batlle@gmail.com>
2020-09-18 17:49:53 -0300

    ej8: energia_subir

--- 00008_Leyendo el estado/extra.py
+++ 00008_Leyendo el estado/extra.py
@@ -3,6 +3,9 @@ Pepita = objeto()
 Pepita.energia = 100
 
 def energia_subir(self, porcentaje):
-  self.energia = self.energia * (1 + porcentaje / 100)  # para subir 25%, multiplicamos por 1.25
+  factor = (1 + porcentaje / 100)  # para subir 25%, multiplicamos por 1.25
+  self.energia *= factor # o sea, self.energia = self.energia * factor
+  self.energia = int(self.energia)
   return self.energia
 
+Pepita.energia_subir = energia_subir
