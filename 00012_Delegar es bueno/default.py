213ba30c18f07bf2b42a312e9414aeed20295430
leandro.batlle@gmail.com <leandro.batlle@gmail.com>
2020-09-25 15:39:07 +0000

    Mumuki Export on 2020-09-25 15:39:07 +0000

--- 00012_Delegar es bueno/default.py
+++ 00012_Delegar es bueno/default.py
@@ -5,38 +5,24 @@ Pepita.energia = 1000
 
 Pepita.ciudad = Oberá
 
-
 def energia_actual(self):
     return self.energia
 
-
 Pepita.energia_actual = energia_actual
-
 
 def ciudad_actual(self):
     return self.ciudad
 
-
 Pepita.ciudad_actual = ciudad_actual
 
-
 def gastar_energia(self, destino):
-    distancia = self.ciudad.distancia_a(destino) #delegamos a  self.ciudad el cálculo de la distancia
-    self.energia -= 0.5 * distancia  #usamos lo que nos diga self.ciudad, ojo con el '-='
-
-
+  self.energia -= 0.5 * (max(self.ciudad.kilómetro(), destino.kilómetro()) - min(self.ciudad.kilómetro(), destino.kilómetro()))
+  
 Pepita.gastar_energia = gastar_energia
 
-
 def volar_hacia(self, destino):
-    self.gastar_energia(destino)
-    self.ciudad = destino
-
+  self.gastar_energia(destino)
+  self.ciudad = destino
 
 Pepita.volar_hacia = volar_hacia
 
-
-def distancia_a(self, destino):
-    # ... completá con tu algoritmo ...
-    # ... recordá que tenés una "ayuda" en el enunciado ...
-    return #... qué devolvemos? ...
--- 00012_Delegar es bueno/description.md
+++ 00012_Delegar es bueno/description.md
@@ -34,7 +34,9 @@ Pepita.volar_hacia = volar_hacia
 ```
 
 > Pero esto se puede mejorar un poco más. Delegá el cálculo de la distancia a los objetos que sean ciudades. El nuevo  método `distancia_a`, toma un destino y devuelve la distancia desde la ciudad `self` hasta el destino.
-> Por ejemplo: 
+> Por ejemplo:   
+
+
 ```Python
 ム Ushuaia.distancia_a(Marambio)
 => 1200
