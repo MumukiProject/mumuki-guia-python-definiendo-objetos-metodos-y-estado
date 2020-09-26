ada3f67b26d755987bf756276a7d75b7590b35a9
Leandro Batlle <leandro.batlle@gmail.com>
2020-09-25 21:06:33 -0300

    no uso sombra_de_pepita

--- 00007_Conociendo el país/test.py
+++ 00007_Conociendo el país/test.py
@@ -1,34 +1,8 @@
-sombra_de_Pepita=pajaritoClass()
-sombra_de_Pepita.energia = 100
-sombra_de_Pepita.ciudad = _Oberá
-Pepita_energia_original = Pepita.energia
-
-#pruebas
-Pepita=pajaritoClass()
-# describe '':
-#   it 'Iruya existe':
-#     expect(Iruya).to be
-# no hace falta
-
-# it 'Obera existe':
-#   expect(Obera).to be
-# no hace falta
-
-# context 'Pepita':
-#   it 'empieza con 100 de energía':
-#     expect(Pepita.energia).to eq 100
-
-
 class Test(unittest.TestCase):
   # it 'empieza en Iruya':
   #     expect(Pepita.ciudad).to eq Iruya
   def test_Pepita_tiene_comenzar_en_Iruya_con_100_de_energia(self):
-    self.assertEqual(sombra_de_Pepita.energia,Pepita.energia,"Pepita.energia vale {} pero debe comenzar con {}".format(Pepita.energia,sombra_de_Pepita.energia))
-    args=dict()
-    args["espero"] = sombra_de_Pepita.ciudad.nombre
-    args["obtengo"] = Pepita.ciudad
-    args["mensaje"] = "Pepita.energia vale {} pero debe comenzar con {}".format(args["obtengo"],args["espero"])
-    self.assertEqual(args["espero"],args["obtengo"],args["mensaje"])
+    self.assertEqual(100,Pepita.energia,"Pepita.energia vale {} pero debe valer {}".format(Pepita.energia,100))
 
   def test_Pepita_entiende_el_mensaje_volar_hacia(self):
     # it 'entiende el mensaje volar_hacia!':
@@ -43,35 +17,22 @@ class Test(unittest.TestCase):
     # it 'pierde 100 unidades de energía cuando vuela':
     #   Pepita.volar_hacia!(Obera)
     #   expect(Pepita.energia).to eq 0
-    ciudades = [_Oberá.nombre, _Iruya.nombre]
-    for miCiudad in ciudades:
-      args=dict()
-      args["member"] = miCiudad
-      args["container"] = dir() #__dict__ customizado
-      args["mensaje"] = "tienen que existir las {} ciudades {}".format(len(ciudades), ciudades)
-      self.assertIn(args["member"],args["container"],args["mensaje"])
-
-    Pepita.volar_hacia(_Oberá.nombre)
-    sombra_de_Pepita.volar_hacia(ciudad_destino=_Oberá,energia_fija=100) #cambia el comportamiento
-    args=dict()
-    args["espero"] = sombra_de_Pepita.ciudad.nombre
-    args["obtengo"] = Pepita.ciudad
-    args["mensaje"] = "Pepita.energia vale {} pero debe comenzar con {}".format(args["obtengo"],args["espero"])
-    self.assertEqual(args["espero"],args["obtengo"],args["mensaje"])
+    Pepita.volar_hacia(Oberá)
+    energía_antes=Pepita.energia
+    Pepita.volar_hacia(Iruya)
+    self.assertEqual(Pepita.energia,energía_antes-100,"al volar de Oberá a Iruya, Pepita.energía, debió perder 100, por ejemplo: pasar de {} a {}".format(Pepita.energia,energía_antes-100) )
+    energía_antes=Pepita.energia
+    Pepita.volar_hacia(Oberá)
+    self.assertEqual(Pepita.energia,energía_antes-100,"al volar de Iruya a Oberá, Pepita.energía, debió perder 100, por ejemplo: pasar de {} a {}".format(Pepita.energia,energía_antes-100) )
 
 
   def test_Pepita_cambia_de_ciudad_cuando_vuela(self):
-    # it 'cambia de ciudad cuando vuela':
-    #   Pepita.volar_hacia!(Obera)
-    #   expect(Pepita.ciudad).to eq Obera
-    ciudades = [_Oberá, _Iruya]
-    for miCiudad in ciudades:
-      sombra_de_Pepita.volar_hacia(miCiudad)
-      Pepita.volar_hacia(miCiudad)
-      args=dict()
-      args["espero"] = sombra_de_Pepita.ciudad.nombre
-      args["obtengo"] = Pepita.ciudad
-      args["mensaje"] = "Pepita.ciudad vale '{obtengo}' pero debe valer '{espero}' luego del mensaje Pepita.volar_hacia({espero})".format(obtengo=args["obtengo"],espero=args["espero"])
-      self.assertEqual(args["espero"],args["obtengo"],args["mensaje"])
-
-
+    """
+    it 'cambia de ciudad cuando vuela':
+      Pepita.volar_hacia!(Iruya)
+      expect(Pepita.ciudad).to eq Iruya
+    """
+    Pepita.volar_hacia(BuenosAires)
+    Pepita.volar_hacia(Iruya)
+    cartel="al recibir el mensaje {nombre}.volar_hacia({destino}), nuestra golondrina tiene que  llegar a {destino}".format(nombre="Pepita",destino=_Iruya.nombre)
+    self.assertEqual(Iruya,Pepita.ciudad_actual(),cartel)
